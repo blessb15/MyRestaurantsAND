9f04b208da7f81370f84bd1126cfbb621cc903dd
blessb15 <blakebless7@gmail.com>
2016-07-06 10:50:56 -0700

    show list of restaurants near and let you scroll though all.

---
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -0,0 +1,79 @@
+package com.epicodus.myrestaurants.models;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+
+@Parcel
+public class Restaurant {
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+    ArrayList<String> mCategories = new ArrayList<>();
+
+
+
+    // empty constructor needed by the Parceler library:
+    public Restaurant() {}
+
+    public Restaurant(String name, String phone, String website,
+                      double rating, String imageUrl, ArrayList<String> address,
+                      double latitude, double longitude, ArrayList<String> categories) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mCategories = categories;
+        mImageUrl = getLargeImageUrl(imageUrl);
+    }
+
+    public String getLargeImageUrl(String ImageUrl){
+        String largeimageUrl = ImageUrl.substring(0, ImageUrl.length() - 6).concat("o.jpg");
+        return largeimageUrl;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getCategories() {
+        return mCategories;
+    }
+}
